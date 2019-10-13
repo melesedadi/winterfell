3a4fcd154b960beb1ca7eca7b7d9b90990f69ac2
chenyuhjiuan <emilymarinovich@hotmail.com>
2019-10-13 14:54:46 -0400

    added th at deptlist

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -85,7 +85,7 @@ public class HomeController {
        // model.addAttribute("employee",employee);
 
 
-       if (file.isEmpty()&&result.hasErrors()){
+       if (file.isEmpty()){
             return "redirect:/addemployee";
         }
         try {
@@ -98,10 +98,10 @@ public class HomeController {
             return "redirect:/addemployee";
         }
 
-        /*if (result.hasErrors()){
+        if (result.hasErrors()){
 
             return "redirect:/addemployee";
-        }*/
+        }
         employeeRepository.save(employee);
 
         return "redirect:/employeelist";
